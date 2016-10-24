66021b3c9f65b4e69b6b3a0eb9ee7e1565f9e5f9
David Martinez Marti <deavidsedice@gmail.com>
2016-10-24 17:40:05 +0200

    mejoras varias

--- www/js/wclicker.js
+++ www/js/wclicker.js
@@ -15,7 +15,16 @@ var renderer = autoDetectRenderer(GAME_WIDTH, GAME_HEIGHT);
 var EARN_SPEED = 1;
 var sprites = new Object();
 
+var frameno = 0;
 var game = new Object();
+
+var isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+    isMobile = true;
+}
+
 
 //PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
@@ -70,7 +79,8 @@ function init_game() {
     stage.addChild(sprites.loader_rect1);
     stage.addChild(sprites.loader_rect2);
 
-    renderer.render(final_stage);
+    renderFrame();
+
     loader.reset();
     loader
       .add( [
@@ -138,7 +148,9 @@ function launchIntoFullscreen(element) {
 }
 
 function title_text_clicked() {
-    launchIntoFullscreen(document.getElementsByTagName("canvas")[0]);
+    if (isMobile) {
+        launchIntoFullscreen(document.getElementsByTagName("canvas")[0]);
+    }
     stage.removeChildren();
     //history.pushState({page: "game"}, "Wizard Clicker - Game", "#game");
 
@@ -154,35 +166,38 @@ function title_text_clicked() {
     sprites.player.x = 50 + 30;
     sprites.player.y = 70 + 30;
 
-    var sphere_separation = GAME_WIDTH * 0.12;
+    var sphere_separation = GAME_WIDTH * 0.17;
     sprites.sphere0 = getSphereSprite("grey");
-    sprites.sphere0.x = GAME_WIDTH * 0.9 - sphere_separation * 0;
+    sprites.sphere0.x = GAME_WIDTH * 0.85 - sphere_separation * 0;
     sprites.sphere0.y = GAME_HEIGHT * 0.1;
 
     sprites.sphere0text = new PIXI.extras.BitmapText("Lvl\n0.00", {font: "32px LatoMediumBold32", align: "center"});
     sprites.sphere0text.anchor.set(0.5, 0.5);
+    sprites.sphere0text.scale.set(0.5, 0.5);
     sprites.sphere0text.x = 0;
     sprites.sphere0text.y = 0;
 
     sprites.sphere0.addChild(sprites.sphere0text);
 
     sprites.sphere1 = getSphereSprite("red");
-    sprites.sphere1.x = GAME_WIDTH * 0.9 - sphere_separation * 1;
+    sprites.sphere1.x = GAME_WIDTH * 0.85 - sphere_separation * 1;
     sprites.sphere1.y = GAME_HEIGHT * 0.1;
 
     sprites.sphere1text = new PIXI.extras.BitmapText("Lvl\n0.00", {font: "32px LatoMediumBold32", align: "center"});
     sprites.sphere1text.anchor.set(0.5, 0.5);
+    sprites.sphere1text.scale.set(0.5, 0.5);
     sprites.sphere1text.x = 0;
     sprites.sphere1text.y = 0;
 
     sprites.sphere1.addChild(sprites.sphere1text);
 
     sprites.sphere2 = getSphereSprite("blue");
-    sprites.sphere2.x = GAME_WIDTH * 0.9 - sphere_separation * 2;
+    sprites.sphere2.x = GAME_WIDTH * 0.85 - sphere_separation * 2;
     sprites.sphere2.y = GAME_HEIGHT * 0.1;
 
     sprites.sphere2text = new PIXI.extras.BitmapText("Lvl\n0.00", {font: "32px LatoMediumBold32", align: "center"});
     sprites.sphere2text.anchor.set(0.5, 0.5);
+    sprites.sphere2text.scale.set(0.5, 0.5);
     sprites.sphere2text.x = 0;
     sprites.sphere2text.y = 0;
 
@@ -194,6 +209,47 @@ function title_text_clicked() {
     stage.addChild(sprites.sphere0);
     stage.addChild(sprites.sphere1);
     stage.addChild(sprites.sphere2);
+
+    sprites.cmd1 = getSphereSprite("empty");
+    sprites.cmd1.x = GAME_WIDTH * 0.15;
+    sprites.cmd1.y = GAME_HEIGHT * 0.9;
+    stage.addChild(sprites.cmd1);
+
+    sprites.cmd1_a1 = getSphereSprite("small_grey");
+    sprites.cmd1_a1.position.set(23,25);
+    sprites.cmd1_a1.scale.set(1.2,1.2);
+    sprites.cmd1.addChild(sprites.cmd1_a1);
+
+    sprites.cmd1_b1 = getSphereSprite("small_red");
+    sprites.cmd1_b1.position.set(23-14,25);
+    sprites.cmd1_b1.scale.set(1.2,1.2);
+    sprites.cmd1.addChild(sprites.cmd1_b1);
+
+    sprites.cmd1_b1t = new PIXI.extras.BitmapText("1",
+            {font: "24px LatoMediumBold24", align: "center"});
+    sprites.cmd1_b1t.anchor.set(0.5, 0.5);
+    sprites.cmd1_b1t.scale.set(0.5,0.5);
+    sprites.cmd1_b1t.x = 2;
+    sprites.cmd1_b1t.y = -1;
+    sprites.cmd1_b1t.alpha = 0.7;
+    //sprites.cmd1_b1.addChild(sprites.cmd1_b1t);
+
+    sprites.cmd1_a1t = new PIXI.extras.BitmapText("3",
+            {font: "24px LatoMediumBold24", align: "center"});
+    sprites.cmd1_a1t.anchor.set(0.5, 0.5);
+    sprites.cmd1_a1t.scale.set(0.5,0.5);
+    sprites.cmd1_a1t.x = 2;
+    sprites.cmd1_a1t.y = -1;
+    sprites.cmd1_a1t.alpha = 0.7;
+    sprites.cmd1_a1.addChild(sprites.cmd1_a1t);
+
+    sprites.cmd1t = new PIXI.extras.BitmapText("Goblin",
+            {font: "32px LatoMediumBold32", align: "center"});
+    sprites.cmd1t.anchor.set(0.5, 0.5);
+    sprites.cmd1t.scale.set(0.5,0.5);
+    sprites.cmd1t.x = 1;
+    sprites.cmd1t.y = 0;
+    sprites.cmd1.addChild(sprites.cmd1t);
 
     game_state = play_game;
     gameLoop();
@@ -214,13 +270,17 @@ function update_mana_levels() {
         return (Math.min(p1,p2)).toFixed(2);
     }
     //console.log(game.mana0 + " " + Math.log(game.mana0+1).toFixed(2));
+    var prev1 = game.lvmana0 + ";" + game.lvmana1 + ";" + game.lvmana2 + ";";
     game.lvmana0 = f(game.mana0, 3);
     game.lvmana1 = f(game.mana1, 4);
     game.lvmana2 = f(game.mana2, 5);
+    var new1 = game.lvmana0 + ";" + game.lvmana1 + ";" + game.lvmana2 + ";";
 
-    sprites.sphere0text.text = "Lvl\n" + game.lvmana0;
-    sprites.sphere1text.text = "Lvl\n" + game.lvmana1;
-    sprites.sphere2text.text = "Lvl\n" + game.lvmana2;
+    if (prev1 != new1) {
+        sprites.sphere0text.text = "Lvl\n" + game.lvmana0;
+        sprites.sphere1text.text = "Lvl\n" + game.lvmana1;
+        sprites.sphere2text.text = "Lvl\n" + game.lvmana2;
+    }
 }
 
 function getCreatureSprite(spritename, onDown) {
@@ -251,11 +311,13 @@ function getCreatureSprite(spritename, onDown) {
 
 function getSphereSprite(spritename) {
     var texture = TextureCache[spritename + "sphere.png"];
-
+    var cnt = new Container();
     var sprite = new Sprite(texture);
     sprite.scale.set(0.5, 0.5)
     sprite.anchor.set(0.5, 0.5);
-    return sprite;
+    cnt.addChild(sprite);
+    cnt.shpere = sprite;
+    return cnt;
 }
 
 function mage_clicked() {
@@ -282,16 +344,16 @@ function play_game() {
 
 
 function gameLoop(){
-
+    frameno +=1;
     //Loop this function 60 times per second
     requestAnimationFrame(gameLoop);
-
+    if (frameno % 3 != 0) return;
     if (game_state) {
         game_state();
     }
 
     //Render the stage
-    renderer.render(stage);
+    renderFrame();
 }
 
 init_game();
